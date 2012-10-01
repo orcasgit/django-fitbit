30d8bd9e8ffd5d263405494e9cfb759879d5e81f
Dan Poirier <dpoirier@caktusgroup.com>
2012-10-01 09:08:45 -0400

    Add setup.py and other files for packaging with python distribute.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+required = [line for line in open('requirements.txt').read().split("\n") if not line.startswith("http")]
+dependency_links = [line for line in open('requirements.txt').read().split("\n") if line.startswith("http")]
+
+setup(
+    name="django-fitbit",
+    version=__import__("fitapp").__version__,
+    author="orcas",
+    author_email="",
+    packages=find_packages(),
+    install_requires=["distribute"] + required,
+    dependency_links=dependency_links,
+    include_package_data=True,
+    url="https://github.com/orcasgit/django-fitbit/",
+    license="",
+    description="Django integration for python-fitbit",
+    long_description=open("README.md").read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2 :: Only"
+    ]
+)
