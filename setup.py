c2972629948463a8017ea161d0fc0e3db6f5e2b7
Dan Poirier <dpoirier@caktusgroup.com>
2012-10-05 09:46:32 -0400

    Install python-fitbit from github using pip.
    It doesn't seem possible to tell setup.py how to clone python-fitbit
    from github. For now, it'll be necessary to "pip install -r requirements.txt"
    to get it.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
-required = [line for line in open('requirements.txt').read().split("\n") if not line.startswith("http")]
-dependency_links = [line for line in open('requirements.txt').read().split("\n") if line.startswith("http")]
+# FIXME: Can't install python-fitbit from setup.py until it's on PyPI
+# For now, use "pip install -r requirements.txt"
+required = ["fitbit"] + \
+           [line for line in open('requirements.txt').read().split("\n") if not line.startswith("git")]
 
 setup(
     name="django-fitbit",
@@ -10,7 +12,6 @@ setup(
     author_email="",
     packages=find_packages(),
     install_requires=["distribute"] + required,
-    dependency_links=dependency_links,
     include_package_data=True,
     url="https://github.com/orcasgit/django-fitbit/",
     license="",
