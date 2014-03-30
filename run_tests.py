664aa26452c4641e391a211163b37e0f905287d8
Brad Pitcher <bradpitcher@gmail.com>
2014-03-30 12:53:49 -0700

    use tox to test many django versions, support django 1.7.x

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+import coverage
 import optparse
+import os
 import sys
 
 from django.conf import settings
+
 
 if not settings.configured:
     settings.configure(
@@ -30,14 +33,41 @@ if not settings.configured:
 
 from django.test.utils import get_runner
 
+
 def run_tests():
     parser = optparse.OptionParser()
-    _, tests = parser.parse_args()
+    parser.add_option('--coverage', dest='coverage', default='2',
+                      help="coverage level, 0=no coverage, 1=without branches,"
+                      " 2=with branches")
+    options, tests = parser.parse_args()
     tests = tests or ['fitapp']
+    
+    covlevel = int(options.coverage)
+    if covlevel:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        if covlevel == 2:
+            branch = True
+        else:
+            branch = False
+        cov = coverage.coverage(branch=branch, config_file='.coveragerc')
+        cov.load()
+        cov.start()
 
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=1, interactive=True, failfast=False)
-    sys.exit(test_runner.run_tests(tests))
+    exit_val = test_runner.run_tests(tests)
+
+    if covlevel:
+        cov.stop()
+        cov.save()
+    
+    sys.exit(exit_val)
+
+
+import django
+# In Django 1.7, we need to run setup first
+if hasattr(django, 'setup'):
+    django.setup()
 
 
 if __name__ == '__main__':
